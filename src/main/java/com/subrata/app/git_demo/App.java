fd996e6e7864a4e1371321e52b07b977fd23139a
subratadebata <debata.subrata@gmail.com>
2018-07-05 15:42:40 +0530

    added if condition to check the data

--- src/main/java/com/subrata/app/git_demo/App.java
+++ src/main/java/com/subrata/app/git_demo/App.java
@@ -9,6 +9,9 @@ public class App
 	
 	public void addData(String data){
 		System.out.println("data::"+data);
+		if(data.equals("java")){
+			System.out.println("java");
+		}
 	}
     public static void main( String[] args )
     {
