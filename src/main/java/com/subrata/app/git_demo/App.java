3dd2d32ec4d789146651ea0748b0ae69f6d0a71b
subratadebata <debata.subrata@gmail.com>
2018-07-06 11:03:19 +0530

    added constant class

--- src/main/java/com/subrata/app/git_demo/App.java
+++ src/main/java/com/subrata/app/git_demo/App.java
@@ -8,8 +8,7 @@ public class App
 {
 	
 	public void addData(String data){
-		System.out.println("data::"+data);
-		if(data.equals("java")){
+		if(data.equals(Constant.DATA_TYPE)){
 			System.out.println("java");
 		}
 	}
---
+++ src/main/java/com/subrata/app/git_demo/Constant.java
@@ -0,0 +1,6 @@
+package com.subrata.app.git_demo;
+
+public class Constant {
+	public static String DATA_TYPE="Subrata";
+
+}
